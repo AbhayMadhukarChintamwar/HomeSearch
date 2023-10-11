660712b6a51cc39ec7c9b860a8d422960135c09f
Abhay218 <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2023-10-11 12:44:31 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
-    },
+        unique: true, 
+    }, 
     email: {
         type: String,
         required: true,
