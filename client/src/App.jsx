83ce308ca72b073e614f03f2c1319c5fac2a7bc3
Abhay218 <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2023-10-21 16:40:13 +0530

    Create Listing Item , create  UpdateListing Item, create  show listing item, create home, about page functioanallity in client side and api .  and so on

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import About from "./pages/about";
-import Signin from "./pages/signin";
-import Home from "./pages/home";
-import Signup from "./pages/signup";
-import Profile from "./pages/profile";
-import Header from "./component/Header";
-import PrivateRout from "./component/privateRout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.jsx';
+import SignIn from './pages/signin.jsx';
+import SignUp from './pages/signup.jsx';
+import About from './pages/about.jsx';
+import Profile from './pages/profile.jsx';
+import Header from './component/Header.jsx';
+import PrivateRoute from './component/privateRout.jsx';
+import CreateListing from './pages/CreateListing.jsx';
+import UpdateListing from './pages/UpdateListing.jsx'
+import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/about" element={<About />} />
-        <Route  element={<PrivateRout />} >
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+         
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
