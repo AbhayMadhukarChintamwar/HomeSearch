a073c80ed63034ee31d4ad4466b25dccb488b311
Abhay218 <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2023-10-22 08:59:10 +0530

    add firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "homesearch-dc5ad.firebaseapp.com",
+  projectId: "homesearch-dc5ad",
+  storageBucket: "homesearch-dc5ad.appspot.com",
+  messagingSenderId: "453111104120",
+  appId: "1:453111104120:web:d845b6cc675bed0556238d"
+};
+
+export const app = initializeApp(firebaseConfig);
